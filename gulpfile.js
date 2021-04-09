9a48a39530138758218343757432dc1b3e915896
Zacharie-William Gagné <zwgagne@outlook.com>
2021-04-09 11:59:28 -0400

    added the gulp-file and the config

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
