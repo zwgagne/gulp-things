d2cc80e96fb53b99853b42dad874c7e0a3f9ecc6
Zacharie-William Gagné <zwgagne@outlook.com>
2021-04-09 12:15:18 -0400

    runned the first gulp task successfully

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,25 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask(){
+    return src('app/scss/style.scss', { sourcemaps: true })
+      .pipe(sass())
+      .pipe(postcss([cssnano()]))
+      .pipe(dest('dist', { sourcemaps: '.' }));
+  }
+
+// JavaScript Task
+function jsTask(){
+    return src('app/js/script.js', { sourcemaps: true })
+      .pipe(terser())
+      .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+
+// Default Gulp task
+exports.default = series(
+    scssTask, 
+    jsTask
+);
