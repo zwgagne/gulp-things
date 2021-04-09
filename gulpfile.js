c7bf39b35098373e2fd14279415edc6ede04e5b2
Zacharie-William Gagné <zwgagne@outlook.com>
2021-04-09 12:30:00 -0400

    took the files from the professor and added/replaced it in the repo

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,44 @@ const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask(){
-    return src('app/scss/style.scss', { sourcemaps: true })
-      .pipe(sass())
-      .pipe(postcss([cssnano()]))
-      .pipe(dest('dist', { sourcemaps: '.' }));
-  }
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }));
+}
 
 // JavaScript Task
 function jsTask(){
-    return src('app/js/script.js', { sourcemaps: true })
-      .pipe(terser())
-      .pipe(dest('dist', { sourcemaps: '.' }));
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }));
 }
 
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '.'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
 
 // Default Gulp task
 exports.default = series(
-    scssTask, 
-    jsTask
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
 );
